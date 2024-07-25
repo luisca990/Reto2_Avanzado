9c3bf1b700b007b368526e55750fba30efe03b42
ErvidMolina89 <eralmoba@hotmail.com>
2024-07-24 21:19:05 -0500

    Implementación de ViewBinding Para las visat de Login, registre y splash

--- ProyectaTe/app/src/main/java/com/example/proyectate/Presentation/AccessAcount/SplashFragment.java
+++ ProyectaTe/app/src/main/java/com/example/proyectate/Presentation/AccessAcount/SplashFragment.java
@@ -5,30 +5,24 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.annotation.NonNull;
 import androidx.navigation.Navigation;
 import com.example.proyectate.Base.BaseFragment;
 import com.example.proyectate.DataAccess.SharedPreferences.SessionManager;
 import com.example.proyectate.R;
-import com.example.proyectate.Utils.Constants;
-
-import java.util.Objects;
+import com.example.proyectate.databinding.FragmentSplashBinding;
 
 public class SplashFragment extends BaseFragment {
     private SessionManager sessionManager;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        setCustomView(inflater.inflate(R.layout.fragment_splash, container, false));
+        FragmentSplashBinding binding = FragmentSplashBinding.inflate(getLayoutInflater());
+        setCustomView(binding.getRoot());
         new Handler().postDelayed(() -> {
             if (sessionManager.isLoggedIn()) {
-                Bundle bundle = new Bundle();
-                if ((Objects.equals(sessionManager.getUserEmail(), Constants.Tag.ADMIN))) {
-                    bundle.putString(Constants.Tag.USER, "admin");
-                } else {
-                    bundle.putString(Constants.Tag.USER, "client");
-                }
-                Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_homeFragment, bundle);
+                Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_homeFragment);
             } else {
                 Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_loginFragment);
             }
